699d96171f6ad349c0426e769ddfcb57f8ebe71b
justisken <144620239+justisken@users.noreply.github.com>
2026-02-12 03:19:29 +0400

    Remove all problematic Google Sheets files - only use HTML code

--- test-deployment.js
+++
@@ -1,4 +0,0 @@
-// Test file to verify deployment
-console.log('Deployment test - new code loaded!');
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbykpFD-LwYU_Osadg2u_fyFKKwCyRGmuT8ILxHqlq-uqgLdwgAuRrtiZjjNiYHwq-WhnA/exec';
-console.log('Google Script URL:', GOOGLE_SCRIPT_URL);
